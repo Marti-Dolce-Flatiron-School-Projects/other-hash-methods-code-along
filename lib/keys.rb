c0e130dd99fd8b189c36604c8f716319e8c96440
Martinique <me@martidolce.com>
2020-11-15 00:19:36 -0500

    commit tree ceb9ff8fb8a2089b59c0bc95de0583ad8359ae15

--- lib/keys.rb
+++ lib/keys.rb
@@ -1,5 +1,10 @@
 require 'pry'
+# frozen_string_literal: true
 
+# Author: Martinique Dolce
+# Course: Flatiron School 2020, November 9 - 20201, April 2021
+# Contact: me@martidolce.com | https://modis.martidolce.com
+#
 #This is the groceries hash we'll be passing in to the get_the_keys method
 # groceries = {
 #  dairy: ["milk", "yogurt", "cheese"],
@@ -9,5 +14,5 @@ require 'pry'
 # }
 
 def get_the_keys(groceries)
-  #code your solution here!
+  groceries.keys
 end
